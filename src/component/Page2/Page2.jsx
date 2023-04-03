ecce7567bdf694d1f43b22a36f49d165b0bea8cf
leonharf <joh.leonhardt@anz.com>
2023-04-03 10:30:01 +1000

    Spiderman component futuristic UI test

---
+++ src/component/Page2/Page2.jsx
@@ -0,0 +1,168 @@
+﻿// import React, { useEffect, useRef } from 'react';
+// import { init } from 'augmented-ui';
+
+
+// const Page4 = () => {
+//   const rootRef = useRef(null);
+
+//   useEffect(() => {
+//     init(rootRef.current);
+
+//   }, []);
+
+//   return (
+//     <div ref={rootRef}>
+//             <div class="metal-holder">
+
+//                 <div 
+//                     data-augmented-ui="tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" 
+//                     class="new_metal1">
+//                 </div>
+//             </div>
+//             <div 
+//                 data-augmented-ui="tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" 
+//                 class="new_metal2">
+//             </div>
+//     </div>
+//   );
+// }
+// export default Page4;
+
+
+
+// import React from "react";
+// import './style.css';
+// import augmentedUi from 'augmented-ui';
+// import React, { useEffect, useRef } from 'react';
+
+
+
+
+// import React, { useEffect } from "react";
+// import { AnzGrid, AnzRow, AnzCol } from '@anz/grid'
+// import Text from '@anz/text'
+
+// import {
+//     StyledDiv
+// } from './styles';
+
+
+// const Page2 = () => {
+
+//     useEffect(
+//         () => {
+//             document.title = 'HWT - Page 4';
+//         }, []
+//     );
+
+//     return (
+//         <StyledDiv>
+//             <AnzGrid fluid>
+//                 <AnzRow center={'xs'}>
+//                     <AnzCol xs={12} sm={12} md={12} lg={12}>
+//                         <div>
+//                             <Text heading='1'>This is Page 4</Text>
+//                         </div>
+//                         <div>
+//                             <Text paragraph='1'>Build components here!</Text>
+//                         </div>
+//                     </AnzCol>
+//                 </AnzRow>
+//             </AnzGrid>
+//         </StyledDiv>
+//     );
+// }
+// export default Page2;
+
+
+
+
+
+// const Page4 = () => {
+
+//     return (
+//         <div>
+//             <div class="metal-holder">
+
+//                 <div 
+//                     data-augmented-ui="tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" 
+//                     class="new_metal1">
+//                 </div>
+//             </div>
+//             <div 
+//                 data-augmented-ui="tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" 
+//                 class="new_metal2">
+//             </div>
+//         </div>
+//     );
+// }
+// export default Page4;
+
+
+
+// import React from "react";
+
+// const Page4 = () => {
+
+//     return (
+//         <div>
+
+//         </div>
+//     );
+// }
+// export default Page4;
+
+
+
+
+// import React from "react";
+// import './style.css';
+// import "augmented-ui/dist/augmented-ui.css";
+// // import "augmented-ui/dist/augmented-ui.js";
+
+// const Page4 = () => {
+
+//     return (
+//         <div>
+//             <div class="metal-holder">
+
+//                 <div data-augmented-ui="
+//   tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" className="new_metal1">
+//                 </div>
+//             </div>
+//             <div data-augmented-ui="
+//   tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" className="new_metal2">
+//             </div>
+//         </div>
+//     );
+// }
+// export default Page4;
+
+
+
+
+import React from "react";
+import './style.css';
+
+const Page4 = () => {
+
+    return (
+        <div>
+<div class="metal-holder">
+ 
+ <div data-augmented-ui="
+   tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" class="new_metal1">
+ </div>
+ </div>
+ <div data-augmented-ui="
+   tr-clip t-clip-x br-clip l-clip-y bl-2-clip-x" class="new_metal2">
+ </div>
+
+
+
+
+        </div>
+    );
+}
+export default Page4;
+
